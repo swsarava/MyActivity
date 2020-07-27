21b394fc644b58134a3029b290303d53722a5521
Tanmay Ghosh <taghos@microsoft.com>
2020-07-27 16:19:03 -0700

    Embedded Data with UI elements

--- myactivity/src/App.js
+++ myactivity/src/App.js
@@ -3,42 +3,84 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fetchData = async () => {
+    try {
+      // TODO: Get current time to identify start and end work hours
+      let getCalendarEventsUrl = "https://graph.microsoft.com/v1.0/me/calendarView?startDateTime=2020-07-27T00:00:00.0000000&endDateTime=2020-07-27T23:59:00.0000000&$select=subject,start,end";
 
-  // TODO: Get current time to identify start and end working time
-  let getCalendarEventsUrl = "https://graph.microsoft.com/v1.0/me/calendarView?startDateTime=2020-07-27T00:00:00.0000000&endDateTime=2020-07-27T23:59:00.0000000&$select=subject,start,end";
-  let addEventUrl = "https://graph.microsoft.com/v1.0/me/calendar/events";
+      // TODO: Call Login API to fetch Auth Token
 
-  // TODO: Call Login API to fetch Auth Token
+      const options = {
+        headers: {
+        "Authorization": "" //Paste Token from Graph Explorer Access Token tab until the generating auth token part is plugged in
+        }
+      };
 
-  var events;
-  fetch(getCalendarEventsUrl, {
-    headers: {
-      "Authorization": "" //Paste Token from Graph Explorer Access Token tab until the generating auth token part is plugged in
-    }
-  }).then((response) => {
-    response.json().then((body) => {
-      events = body.value;
-      console.log(events);
+      // Fetching user's calendar data for today
+      var response = await fetch(getCalendarEventsUrl, options);
+      if(response.status >= 300) {
+        throw new Error(response.statusText);
+      }
+
+      var body = await response.json();
+      var events = body.value;
       events.forEach(element => {
         console.log(element.subject + " : " + element.start.dateTime + " to " + element.end.dateTime);
       });
-      // TODO: Scheduling algorithm to indentify free times from the calendar
-    }).catch((err) => {
-      console.log("Error in parsing json response: " + err);
-    });
-  }).catch((error) => {
-    console.log("Error in calling graph API: " + error.message);
-  });
 
-  // TODO: Create calendar event when a free time is identified
-  
+      // TODO: Scheduling algorithm to indentify free times from the calendar
+    } catch (error) {
+      console.log("Error in fetching calendar data: " + error.message);
+    }
+  }
+    
+  const createEvent = async () => {
+    try {
+      let addEventUrl = "https://graph.microsoft.com/v1.0/me/events";
+
+      // TODO: Call Login API to fetch Auth Token
+
+      const options = {
+        method: "POST",
+        body: JSON.stringify(
+          {  
+            subject: "Some Activity",
+            start: {
+              dateTime: "2020-07-27T17:00:00.000",
+              timeZone: "Pacific Standard Time"
+            },
+            end: {
+              dateTime: "2020-07-27T17:30:00.000",
+              timeZone: "Pacific Standard Time"
+            },
+            responseRequested: true,
+            allowNewTimeProposals: true
+          }
+        ),
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": "" //Paste Token from Graph Explorer Access Token tab until the generating auth token part is plugged in
+        }
+      };
+
+      // TODO: Create calendar event for an identified free time
+      var response = await fetch(addEventUrl, options);
+      console.log(response);
+      if(response.status >= 300) {
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      console.log("Error in creating calendar event: " + error.message);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -47,6 +89,15 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <button id="fetch" onClick={fetchData}>
+            Fetch My Calendar Data
+          </button>
+          <br/>
+          <button id="create" onClick={createEvent}>
+            Create an Event in my Calendar
+          </button>
+        </div>
       </header>
     </div>
   );
