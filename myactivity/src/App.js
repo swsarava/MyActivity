5f2c674ac7e1d77c8746bcbf338ea645366b32a0
Tanmay Ghosh <taghos@microsoft.com>
2020-07-27 15:39:27 -0700

    Fetching calendar events

--- myactivity/src/App.js
+++ myactivity/src/App.js
@@ -3,6 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // TODO: Get current time to identify start and end working time
+  let getCalendarEventsUrl = "https://graph.microsoft.com/v1.0/me/calendarView?startDateTime=2020-07-27T00:00:00.0000000&endDateTime=2020-07-27T23:59:00.0000000&$select=subject,start,end";
+  let addEventUrl = "https://graph.microsoft.com/v1.0/me/calendar/events";
+
+  // TODO: Call Login API to fetch Auth Token
+
+  var events;
+  fetch(getCalendarEventsUrl, {
+    headers: {
+      "Authorization": "" //Paste Token from Graph Explorer Access Token tab until the generating auth token part is plugged in
+    }
+  }).then((response) => {
+    response.json().then((body) => {
+      events = body.value;
+      console.log(events);
+      events.forEach(element => {
+        console.log(element.subject + " : " + element.start.dateTime + " to " + element.end.dateTime);
+      });
+      // TODO: Scheduling algorithm to indentify free times from the calendar
+    }).catch((err) => {
+      console.log("Error in parsing json response: " + err);
+    });
+  }).catch((error) => {
+    console.log("Error in calling graph API: " + error.message);
+  });
+
+  // TODO: Create calendar event when a free time is identified
+  
   return (
     <div className="App">
       <header className="App-header">
